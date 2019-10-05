6629f95a6c1758e4cd4efd51c32618f6abd39bdd
John Lewis <johnlewis@MacBook-Pro.local>
2019-10-04 19:43:41 -0600

    first changes

--- for-in-delete.js
+++ for-in-delete.js
@@ -40,7 +40,11 @@
 */
 
 function showValues( obj ) {
-  //Code Here
+  let sum = ''
+  for(var key in obj){
+    sum += obj[key]
+  }
+  return sum 
 }
 
 
@@ -54,7 +58,15 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function greaterThan10(obj){
+  for (var key in obj){
+    if(obj[key]> 10){
+      obj[key] = 0
+    }
+    
+  }
+  return obj
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -66,7 +78,12 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function double(obj){
+  for (var key in obj){
+    obj[key] * 2
+  }
+  return obj
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -80,7 +97,17 @@ function showValues( obj ) {
 */
 
 //Code Here
+function secrets(obj){
+  let beQuiet = ''
+  var toSearch = 'sh'
+  for (var key in obj){
+    if(obj[key].indexOf(toSearch) != -1){
+      beQuiet += obj[key]
+    }
 
+  }
+  return beQuiet 
+}
 
 
 /* 
